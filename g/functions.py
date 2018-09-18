1a69f3e5de6d61a3cf2e0106ca1b870192ac97a4
Jannis <j.m.hoch@uu.nl>
2018-09-18 16:50:06 +0200

    changed test functions

--- g/functions.py
+++ g/functions.py
@@ -3,7 +3,9 @@
 import numpy as np
 
 def create_model_output():
+    # model_a_out = np.random.rand()
     model_a_out = np.random.rand()
-    model_b_out = np.random.rand()
+    # model_b_out = np.random.rand()
+    model_b_out = model_a_out * 2
     return model_a_out, model_b_out
     
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -13,6 +13,6 @@ def test_input_is_output():
     model_a_out, model_b_out = fn.create_model_output()
     print str(model_a_out)
     print str(model_b_out)
-    assert model_a_out == model_b_out, "output from model a and b"
+    assert model_a_out == 0.5 * model_b_out, "output from model a is not half of output from model b"
     print 'hello World'
     
